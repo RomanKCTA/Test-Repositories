e77dc67f151baa833380358e73a56e581603cb83
Roman Kamalov <Kadria1183@gmail.com>
2021-10-05 20:16:33 +0300

    Normal version 1.0

--- main.py
+++ main.py
@@ -5,17 +5,13 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(names):
-    # Use a breakpoint in the code line below to debug your script.
-<<<<<<< HEAD
-    print(f'Hi, my {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-=======
-    print(f'Hi, {names}')  # Press Ctrl+F8 to toggle the breakpoint.
->>>>>>> aae6f3df41c9059fd900aee14c0810d02e0ce0ed
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.<<<<<<< HEAD
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.=======
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.>>>>>>> aae6f3df41c9059fd900aee14c0810d02e0ce0ed
 
 
-# Press the green
-button in the gutter to run the script.
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
 
